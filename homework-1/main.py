32e75da5dda6e69f34d5375dc56bc11c32b06f70
NasirovEF <i@ef-nasirov.ru>
2024-03-31 16:34:52 +0300

    add skript to filling the table with data

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
+with conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', encoding='utf-8') as file:
+            header = next(file)
+            file_r = csv.reader(file, delimiter=',')
+            for v in file_r:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", tuple(v))
+
+        with open('north_data/employees_data.csv', encoding='utf-8') as file:
+            header = next(file)
+            file_r = csv.reader(file, delimiter=',')
+            for v in file_r:
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", tuple(v))
+
+        with open('north_data/orders_data.csv', encoding='utf-8') as file:
+            header = next(file)
+            file_r = csv.reader(file, delimiter=',')
+            for v in file_r:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", tuple(v))
+
+conn.close()
